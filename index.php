0acb9bf8db0fe1cba488076fc508d3cec5d7734e
Jessica May Ubaldo <jubaldo@csumb.edu>
2018-02-05 16:46:01 +0000

    pushing to two repos

--- index.php
+++ index.php
@@ -1,9 +1,16 @@
-<!DOCTYPE html>
+<?php<!DOCTYPE html>
 <html>
     <head>
         <title> </title>
     </head>
     <body>
-        <?php include_once("index.html"); ?>
+        //  include_once("index.html"); 
+        <div>
+            <h1>Labs</h1>
+            <!--/ means start at root directory-->
+            <a href="/Labs/Personal_Portfolio">Lab 1</a>
+            <!--no / means "start here", which is still the root directory-->
+            <a href="Labs">Labs</a>
+        </div>
     </body>
-</html>
+</html>?>
